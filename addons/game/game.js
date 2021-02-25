dbc248f7a549abb3798d2a2b202fb849a40e0d5e
Malelus <matin1608@gmail.com>
2021-02-25 01:50:16 +0100

    Minor changes

--- addons/game/game.js
+++ addons/game/game.js
@@ -9,7 +9,7 @@ const gameBoard = document.getElementById('gameBoard');
 function main(currentTime) {
   if (gameOver) {
     if (confirm('You lost. Press ok to restart.')) {
-      window.location = '/';
+      location.reload();
     }
     return;
   }
